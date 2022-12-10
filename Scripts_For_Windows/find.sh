33de84a455597344c6ffa438addd533144ddcad8
Tsaihemanth150 <90019941+Tsaihemanth150@users.noreply.github.com>
2022-12-10 20:18:42 +0530

    Update find.sh

--- Scripts_For_Windows/find.sh
+++ Scripts_For_Windows/find.sh
@@ -6,7 +6,10 @@ arp -a
 
 echo "Find out the route of the routing for "
 read input
-tracert $input
+tracert $input >> output.txt ## the output is stored in the file.
 
+
+
+## please check the output file in pwd(present working directorty).
 
  # the shot script to find the basic details like internet connectivity, ARP Table, and Routing route;
