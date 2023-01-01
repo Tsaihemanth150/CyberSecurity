8a872fa7e48b8f8719c5313f21c266affa18ba29
Tsaihemanth150 <90019941+Tsaihemanth150@users.noreply.github.com>
2023-01-01 10:37:05 +0530

    Update PhoneNumberFinder.py

--- PhoneNumberFinder.py
+++ PhoneNumberFinder.py
@@ -61,7 +61,28 @@ report.write(strpossible)
 report.write("\n")
 report.write("\n")
 report.close()
+########################################################
+def convert_file(file):
+    pdf = FPDF()
+    pdf.add_page()
+
+    for text in file:
+        if len(text) <= 20:
+            pdf.set_font("Arial","B",size=18) # For title text
+            pdf.cell(w=200,h=10,txt=text,ln=1,align="C")
+        else:
+            pdf.set_font("Arial",size=15) # For paragraph text
+            pdf.multi_cell(w=0,h=10,txt=text,align="L")
+    pdf.output("output.pdf")
+    print("Successfully converted!")
+file = open("output.txt","r")
+
+convert_file(file)
+
+
+
 #################################################
 # requried package is phonenumbers
-# installtion:- pip install phonenumbers
+# installtion:- pip install phonenumbers, FPDF
 
+#function to convert the TXT to PDF
